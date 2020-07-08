f3e671a94ae0edbfeeafb5a6c6fc158e0ee3fbcc
pacodrummer <pacodrummer@gmail.com>
2020-07-08 13:29:53 +0200

    Se creo archivo programa_003_strings.py

---
+++ programa_003_strings.py
@@ -0,0 +1,51 @@
+mensaje = "Hola"
+mensaje += " "
+mensaje += "Paco"
+
+print(mensaje)
+
+
+print("Concatenacion:")
+
+mensaje = "Hola"
+espacio = " " 
+nombre = "Paco"
+
+print(mensaje + espacio + nombre)
+
+print("Ahora Concatenacion con Strings y Numeros")
+numero_uno = 4
+numero_dos = 6
+
+resultado = numero_uno + numero_dos
+resultado = str(resultado)
+
+print("El resultado de sumar 4 y 6 es " + resultado)
+
+
+print("Metodo de busquedad con find")
+
+mensajaso = "Paco, Paquito, Paquete, Pacoso, Pacorro, Paconan"
+
+buscar_sub = mensajaso.find("Paconan")
+ 
+print(buscar_sub)
+
+
+ 
+print("Extraccion")
+
+mensaj="Hola Paconan"
+
+extraer_sub=mensaj[1:8]
+
+print(extraer_sub)
+
+print("Comparando")
+
+uno = "Hola"
+dos = "Hola"
+
+print(uno == dos)
+
+ 
